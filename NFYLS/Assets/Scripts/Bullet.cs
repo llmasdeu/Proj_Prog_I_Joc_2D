8f6ada49639d79e7888b169bc4e2adbd4e679b50
llmasdeu <ls31344@salleurl.edu>
2017-01-03 23:20:42 +0100

    Afegida la carpeta amb el projecte

---
+++ NFYLS/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour 
+{
+	public float m_speed = 0.5f;
+
+	void Start () 
+	{
+		Destroy (gameObject, 3.0f);
+	}
+
+	void Update () 
+	{
+		transform.position += transform.right * m_speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "enemy") 
+		{
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ NFYLS/Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour 
+{
+	public GameObject m_bullet;
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			fire();
+		}
+
+	}
+
+	private void fire()
+	{
+		Vector3 mouse = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+		GameObject bullet = Instantiate (m_bullet) as GameObject;
+		bullet.transform.position = transform.position;
+
+		float rad = Mathf.Atan2 (mouse.y - transform.position.y, mouse.x - transform.position.x);
+		bullet.transform.Rotate (new Vector3(0,0, Mathf.Rad2Deg * rad));
+	}
+}
