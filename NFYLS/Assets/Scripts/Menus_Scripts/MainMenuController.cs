eb3745641743d31696ec7cb8327fa803d62015a0
Lluís Masdeu <lluismasdeu@MacBook-Pro-de-Lluis.local>
2017-07-10 19:32:29 +0200

    Completado el menú principal del juego. Creado (a completar) el menú de selección de nivel.

---
+++ NFYLS/Assets/Scripts/Menus_Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+	public void OnNewGameButtonClick() {
+		SceneManager.LoadScene ("Level 1");
+	}
+
+	public void OnExitGameButtonClick() {
+		Application.Quit ();
+	}
+}
