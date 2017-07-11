10d0bbfe7169f53521df4bc9ac03f1ed1bc63362
Lluís Masdeu <lluismasdeu@MacBook-Pro-de-Lluis.local>
2017-07-11 12:43:59 +0200

    Completada la escena de GAME OVER. Comprobado su correcto funcionamiento.

---
+++ NFYLS/Assets/Scripts/Menus_Scripts/GameOverMenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenuController : MonoBehaviour {
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene ("Main Menu");
+		}
+	}
+}
